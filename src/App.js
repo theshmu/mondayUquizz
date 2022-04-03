8995c1a541f1fc762bdf225a9b3080dfebc53e23
Shmuel Cohen <cohen.shmuel@gmail.com>
2022-04-03 19:14:16 +0300

    updated so the answers do not shuffle after pressed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import { Questionaire } from './components'
+
+const API_URL = 'https://opentdb.com/api.php?amount=100';
+const quizLength = 10;
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [questions, setQuestions] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [score, setScore] = useState((0));
+    const [showAnswers, setShowAnswers] = useState(false);
+
+    useEffect(() => {
+        fetch(API_URL)
+            .then(res => res.json())
+            .then((data) => {
+                setQuestions(data.results);
+                const questions = data.results.map((question) =>
+                    ({
+                       ...question, answers: [question.correct_answer, ...question.incorrect_answers]
+                            .sort(() => Math.random() - 0.5)
+                    }))
+
+                setQuestions(questions);
+            });
+    }, []);
+
+    const handleAnswer = (answer) => {
+        if(!showAnswers) {
+            if (answer === questions[currentIndex].correct_answer) {
+                setScore(score + 1);
+            }
+        }
+        setShowAnswers(true);
+
+        // check for the answer
+        // show another question
+        // change score if correct
+    };
+
+    const handleNextQuestion = () => {
+        setShowAnswers(false);
+
+        setCurrentIndex(currentIndex + 1);
+    }
+
+  return questions.length > 0 ? (
+      <div className='container'>
+          {currentIndex >= quizLength ? (
+      <h1 className={'text-3xl text-white font-bold'}>Game Over! Your score was: {score/quizLength*100}%</h1>
+      ) : (
+              <Questionaire data={questions[currentIndex]}
+                            handleNextQuestion={handleNextQuestion}
+                            handleAnswer={handleAnswer}
+                            showAnswers={showAnswers}/>
+          )}
+      </div>
+     ) : (
+        <h2 className={'text-2xl text-white font-bold'}>Loading...</h2>
+
+     );
 }
 
 export default App;
