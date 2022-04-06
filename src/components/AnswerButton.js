fa5fff810bcd8e018c16526cc7989e9b96e7b45a
Shmuel Cohen <cohen.shmuel@gmail.com>
2022-04-06 16:22:16 +0300

    adjusted selected button border width

--- src/components/AnswerButton.js
+++ src/components/AnswerButton.js
@@ -10,18 +10,15 @@ export class AnswerButton extends Component {
     render() {
         return (
             <Button
-                //key={this.props.key}
                 onClick={() => {
-                    console.log(this.props.buttonIndex);
                     if(!this.props.clicked) {
                         this.props.setButtonIndex(this.props.index);
                     }
                     this.props.handleAnswer(this.props.answer);
                     clearInterval(this.props.interval);
-                    console.log(this.props.buttonIndex);
                 }}
                 className={this.props.index === this.props.buttonIndex ?
-                    `bg-white ${this.props.textColor} p-4 border-8 border-black font-semibold rounded hover:bg-blue-100`
+                    `bg-white ${this.props.textColor} p-4 border-4 border-black font-semibold rounded hover:bg-blue-100`
                 : `bg-white ${this.props.textColor} p-4 border-0 font-semibold rounded hover:bg-blue-100`}
                 dangerouslySetInnerHTML={{__html: this.props.answer}}/>
         );
